245830c3db6ef14eb24d0ccc1247adcde3f122fe
taho7766 <wjtw9802@icloud.com>
2024-03-15 18:09:47 -0600

    fixed wrong icon on sidenav

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -47,13 +47,13 @@ const Sidenav = () => {
                         <AiOutlineHome size={20}/>
                     </a>
                     <a href='#about' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <GrProjects size={20}/>
+                        <BsPerson size={20}/>
                     </a>
                     <a href='#projects' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <AiOutlineProject size={20}/>
                     </a>
                     <a href='#resume' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <BsPerson size={20}/>
+                        <GrProjects size={20}/>
                     </a>
                     <a href='#contact' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <AiOutlineMail size={20}/>
