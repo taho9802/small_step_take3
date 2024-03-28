9be61335d6f1435c2cb3fbee3c3b3bf5d6b39131
taho7766 <wjtw9802@icloud.com>
2024-03-28 16:28:15 -0600

    added resume link

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlineMail } from 'react-icons/ai'
 import { BsPerson } from 'react-icons/bs'
 import { GrProjects } from 'react-icons/gr'
+import Resume from '../assets/Hong_Taewoo_Resume.pdf'
 
 const Sidenav = () => {
 
@@ -22,13 +23,13 @@ const Sidenav = () => {
                         </a>
                         <a onClick={handleNav} href='#about' className='w-[75%] flex justify-center items-center rounded-full show-lg bg-gray-200 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 hover:bg-gray-300'>
                             <BsPerson size={20} />
-                            <span className='pl-4'>about</span>
+                            <span className='pl-4'>About</span>
                         </a>
                         <a onClick={handleNav} href='#projects' className='w-[75%] flex justify-center items-center rounded-full show-lg bg-gray-200 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 hover:bg-gray-300'>
                             <AiOutlineProject size={20} />
                             <span className='pl-4'>Projects</span>
                         </a>
-                        <a onClick={handleNav} href='#resume' className='w-[75%] flex justify-center items-center rounded-full show-lg bg-gray-200 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 hover:bg-gray-300'>
+                        <a onClick={handleNav} href={Resume} rel='noopener noreferrer' target='_blank' className='w-[75%] flex justify-center items-center rounded-full show-lg bg-gray-200 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 hover:bg-gray-300'>
                             <GrProjects size={20} />
                             <span className='pl-4'>Resume</span>
                         </a>
@@ -43,19 +44,19 @@ const Sidenav = () => {
             }
             <div className='md:block hidden fixed top-[50%] translate-y-[-50%] right-4 z-10'>
                 <div className='flex flex-col'>
-                    <a href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <a title='Home' href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <AiOutlineHome size={20}/>
                     </a>
-                    <a href='#about' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <a title='About' href='#about' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <BsPerson size={20}/>
                     </a>
-                    <a href='#projects' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <a title='Projects' href='#projects' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <AiOutlineProject size={20}/>
                     </a>
-                    <a href='#resume' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <a title='Resume' href={Resume} rel='noopener noreferrer' target='_blank'  className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <GrProjects size={20}/>
                     </a>
-                    <a href='#contact' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <a title='Contact' href='#contact' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <AiOutlineMail size={20}/>
                     </a>
                 </div>
